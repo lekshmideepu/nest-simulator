69aa6efb037521a9be65456c5508ee26c715fd31
Rajalekshmi Deepu <r.deepu@fz-juelich.de>
2017-06-30 10:03:08 +0200

    corrected the syntax

--- music_install.sh
+++ music_install.sh
@@ -3,15 +3,15 @@
 if [ xMUSIC=1 ]; then
 
     # Install current MUSIC version.
-    - git clone https://github.com/INCF/MUSIC.git music
-    - cd music
-    - ./autogen.sh
-    - ./configure --prefix=$HOME/.cache/music.install
-    - make
-    - make install
-    - cd $HOME/build
+   git clone https://github.com/INCF/MUSIC.git music
+   cd music
+   ./autogen.sh
+   ./configure --prefix=$HOME/.cache/music.install
+   make
+   make install
+   cd $HOME/build
 
   # Change directory back to the NEST source code directory.
-   - cd $SOURCEDIR
+   cd $SOURCEDIR
    
 fi
