11b995cd750be088e983b7a4a5c68de4b63c219d
Rajalekshmi Deepu <r.deepu@fz-juelich.de>
2018-10-29 19:08:06 +0100

    checking the static code analysis flag

--- extras/travis_build.sh
+++ extras/travis_build.sh
@@ -107,65 +107,66 @@ echo "+ + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + 
 echo "+               S T A T I C   C O D E   A N A L Y S I S                       +"
 echo "+ + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + +"
 
-echo "MSGBLD0010: Initializing VERA++ static code analysis."
-wget --no-verbose https://bitbucket.org/verateam/vera/downloads/vera++-1.3.0.tar.gz
-tar -xzf vera++-1.3.0.tar.gz
-cd vera++-1.3.0
-cmake -DCMAKE_INSTALL_PREFIX=/usr -DVERA_LUA=OFF -DVERA_USE_SYSTEM_BOOST=ON
-sudo make install
-cd ..
-rm -fr ./vera++-1.3.0
-rm -f ./vera++-1.3.0.tar.gz
- # Add the NEST profile to the VERA++ profiles.
-sudo cp ./extras/vera++.profile /usr/lib/vera++/profiles/nest
-echo "MSGBLD0020: VERA++ initialization completed."
-if [ ! -f "$HOME/.cache/bin/cppcheck" ]; then
-    echo "MSGBLD0030: Installing CPPCHECK version 1.69."
-    # Build cppcheck version 1.69
-    git clone https://github.com/danmar/cppcheck.git
-    cd cppcheck
-    git checkout tags/1.69
-    mkdir -p install
-    make PREFIX=$HOME/.cache CFGDIR=$HOME/.cache/cfg HAVE_RULES=yes install
+if [ "$xSTATIC_ANALYSIS" = "1" ] ; then
+    echo "MSGBLD0010: Initializing VERA++ static code analysis."
+    wget --no-verbose https://bitbucket.org/verateam/vera/downloads/vera++-1.3.0.tar.gz
+    tar -xzf vera++-1.3.0.tar.gz
+    cd vera++-1.3.0
+    cmake -DCMAKE_INSTALL_PREFIX=/usr -DVERA_LUA=OFF -DVERA_USE_SYSTEM_BOOST=ON
+    sudo make install
     cd ..
-    echo "MSGBLD0040: CPPCHECK installation completed."
+    rm -fr ./vera++-1.3.0
+    rm -f ./vera++-1.3.0.tar.gz
+   # Add the NEST profile to the VERA++ profiles.
+    sudo cp ./extras/vera++.profile /usr/lib/vera++/profiles/nest
+    echo "MSGBLD0020: VERA++ initialization completed."
+    if [ ! -f "$HOME/.cache/bin/cppcheck" ]; then
+       echo "MSGBLD0030: Installing CPPCHECK version 1.69."
+       # Build cppcheck version 1.69
+       git clone https://github.com/danmar/cppcheck.git
+       cd cppcheck
+       git checkout tags/1.69
+       mkdir -p install
+       make PREFIX=$HOME/.cache CFGDIR=$HOME/.cache/cfg HAVE_RULES=yes install
+       cd ..
+       echo "MSGBLD0040: CPPCHECK installation completed."
 
-    echo "MSGBLD0050: Installing CLANG-FORMAT."
-    wget --no-verbose http://llvm.org/releases/3.6.2/clang+llvm-3.6.2-x86_64-linux-gnu-ubuntu-14.04.tar.xz
-    tar xf clang+llvm-3.6.2-x86_64-linux-gnu-ubuntu-14.04.tar.xz
-    # Copy and not move because '.cache' may aleady contain other subdirectories and files.
-    cp -R clang+llvm-3.6.2-x86_64-linux-gnu-ubuntu-14.04/* $HOME/.cache
-    echo "MSGBLD0060: CLANG-FORMAT installation completed."
+       echo "MSGBLD0050: Installing CLANG-FORMAT."
+       wget --no-verbose http://llvm.org/releases/3.6.2/clang+llvm-3.6.2-x86_64-linux-gnu-ubuntu-14.04.tar.xz
+       tar xf clang+llvm-3.6.2-x86_64-linux-gnu-ubuntu-14.04.tar.xz
+       # Copy and not move because '.cache' may aleady contain other subdirectories and files.
+       cp -R clang+llvm-3.6.2-x86_64-linux-gnu-ubuntu-14.04/* $HOME/.cache
+       echo "MSGBLD0060: CLANG-FORMAT installation completed."
 
-    # Remove these directories, otherwise the copyright-header check will complain.
-    rm -rf ./cppcheck
-    rm -rf ./clang+llvm-3.6.2-x86_64-linux-gnu-ubuntu-14.04
-fi
+       # Remove these directories, otherwise the copyright-header check will complain.
+       rm -rf ./cppcheck
+       rm -rf ./clang+llvm-3.6.2-x86_64-linux-gnu-ubuntu-14.04
+    fi
 
-# Ensure that the cppcheck and clang-format installation can be found.
-export PATH=$HOME/.cache/bin:$PATH
+    # Ensure that the cppcheck and clang-format installation can be found.
+    export PATH=$HOME/.cache/bin:$PATH
 
-echo "MSGBLD0070: Retrieving changed files."
-  # Note: BUG: Extracting the filenames may not work in all cases. 
-  #            The commit range might not properly reflect the history.
-  #            see https://github.com/travis-ci/travis-ci/issues/2668
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-   echo "MSGBLD0080: PULL REQUEST: Retrieving changed files using GitHub API."
-   file_names=`curl "https://api.github.com/repos/$TRAVIS_REPO_SLUG/pulls/$TRAVIS_PULL_REQUEST/files" | jq '.[] | .filename' | tr '\n' ' ' | tr '"' ' '`
-else
-   echo "MSGBLD0090: Retrieving changed files using git diff."    
-   file_names=`(git diff --name-only $TRAVIS_COMMIT_RANGE || echo "") | tr '\n' ' '`
-fi
-
-printf '%s\n' "$file_names" | while IFS= read -r line
- do
-   for single_file_name in $file_names
-   do
-     echo "MSGBLD0095: File changed: $single_file_name"
-   done
- done
-echo "MSGBLD0100: Retrieving changed files completed."
-echo
+    echo "MSGBLD0070: Retrieving changed files."
+    # Note: BUG: Extracting the filenames may not work in all cases. 
+    #            The commit range might not properly reflect the history.
+    #            see https://github.com/travis-ci/travis-ci/issues/2668
+    if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+       echo "MSGBLD0080: PULL REQUEST: Retrieving changed files using GitHub API."
+       file_names=`curl "https://api.github.com/repos/$TRAVIS_REPO_SLUG/pulls/$TRAVIS_PULL_REQUEST/files" | jq '.[] | .filename' | tr '\n' ' ' | tr '"' ' '`
+    else
+       echo "MSGBLD0090: Retrieving changed files using git diff."    
+       file_names=`(git diff --name-only $TRAVIS_COMMIT_RANGE || echo "") | tr '\n' ' '`
+    fi
+ 
+    printf '%s\n' "$file_names" | while IFS= read -r line
+    do
+       for single_file_name in $file_names
+          do
+            echo "MSGBLD0095: File changed: $single_file_name"
+          done
+    done
+    echo "MSGBLD0100: Retrieving changed files completed."
+    echo
 
 
 # Set the command line arguments for the static code analysis script and execute it.
@@ -200,6 +201,9 @@ chmod +x ./extras/static_code_analysis.sh
 "$IGNORE_MSG_VERA" "$IGNORE_MSG_CPPCHECK" "$IGNORE_MSG_CLANG_FORMAT" "$IGNORE_MSG_PEP8"
 
 # End of Static code analysis.
+else
+    echo "MSGBLD0100: Static code check is not enabled.""
+fi
 
 cd "$NEST_VPATH"
 cp ../examples/sli/nestrc.sli ~/.nestrc
