79ff0bcce2863ff08c73a9709b1bb924d9f2e1e9
Rajalekshmi Deepu <r.deepu@fz-juelich.de>
2019-04-10 17:09:03 +0200

    checking for ostype and using readlink accordingly

--- extras/travis_build.sh
+++ extras/travis_build.sh
@@ -97,6 +97,7 @@ fi
 
 NEST_VPATH=build
 NEST_RESULT=result
+if [[ $OSTYPE == darwin* ]]; then NEST_RESULT=$(greadlink -f $NEST_RESULT);fi
 NEST_RESULT=$(readlink -f $NEST_RESULT)
 
 mkdir "$NEST_VPATH" "$NEST_RESULT"
